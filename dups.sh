f4589c84f8f894c3f46db0d15219404e57353890
webtaster <jim_mcdonnell_@hotmail.com>
2017-03-20 16:26:02 +0000

    Comments added

--- dups.sh
+++ dups.sh
@@ -5,10 +5,12 @@
 # v 1.2
 #
 
+# Get a list of files with non-unique sizes, ie. potential duplicates
 echo Running find... >&2
 find . -type f -size +0 -printf "%-25s %p\n" | sort -n | uniq -D -w 25  > /tmp/dupsizes.$$
 
 
+# Checksum potential duplicate files, discard non-duplicates
 echo Calculating $(wc -l < /tmp/dupsizes.$$) check sums... >&2
 cat /tmp/dupsizes.$$ |
    sed 's/^\w* *\(.*\)/md5sum "\1"/' | 
@@ -17,6 +19,7 @@ cat /tmp/dupsizes.$$ |
             uniq -w32 --all-repeated=separate  > /tmp/dups.$$
 
 
+# Print list of duplicated files, grouped.
 echo Found $(grep -c . /tmp/dups.$$) duplicated files
 while read md5 filename
 do
